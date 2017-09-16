15d3b7d35d197fa5f716ab9edaa309c242225a46
shahriarcse <hosen.shahriar.cse@gmail.com>
2017-09-16 14:17:29 +0600

    user data delete done

--- admin/delete-user.php
+++ admin/delete-user.php
@@ -1,3 +1,7 @@
 <?php
-
+	require_once('functions/admin-functions.php');
+	$id = $_REQUEST['deleteId'];
+	$query = "DELETE FROM tbl_user WHERE id='$id' ";
+	$sqlQuery = mysqli_query($dbconnect,$query);
+	header('Location:all-user.php');
 ?>
