27d5d7466dcb7e0760165727755978018ee645af
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-10-16 12:15:22 +0600

    School Template

--- admin/delete-user.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once('functions/admin-functions.php');
-	$id = $_REQUEST['deleteId'];
-	$query = "DELETE FROM tbl_user WHERE id='$id' ";
-	$sqlQuery = mysqli_query($dbconnect,$query);
-	header('Location:all-user.php');
-?>
--- admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('functions/admin-functions.php');
-    needLogIn();
-    getAdminHeader();
-    getAdminSidebar(); 
-    getBreadcrum();
-    getAdminPart('dashboard.php');
-    getAdminFooter();
-?>    
